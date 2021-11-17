837ae537407a03d2580f6b722511fdc700848372
Son N. Nguyen <namson98@gmail.com>
2021-11-17 14:48:27 +0100

    init cloud class

--- encryption/cryptography.R
+++ encryption/cryptography.R
@@ -16,11 +16,14 @@ print(paste("Number of possible hashes:", 16^nchar(hashed.message))) # Why? 16: 
 # Hash collision? Not likely. Check the last chart in the URL below. We are using a 64*8=
 # : https://preshing.com/20110504/hash-collision-probabilities/
 
+#more secure to append a string every time
+digest(paste('mypwd', "service_specific_secret_salt"), algo="sha256")
 
 # Encryptions
 # ===========
 
-# Symmetric encryption: Caesar
+# Symmetric encryption: Caesar (uses the same key for encoding and decoding)
+# shifting letters
 # ----------------------------
 
 install.packages("caesar")
@@ -32,8 +35,16 @@ decoded = caesar(coded, shift=1, decrypt = TRUE)
 print(paste("Decoded message:", decoded))
 
 
-# Asymmetric / Public Key Encryption (PKI)
+# Asymmetric / Public Key Encryption (PKI) (separate key for encoding and decoding --> generating pair of keys)
 # ---------------------
+# 
+# Key 1 -> Private Key # For decryption
+# Key 2 -> Public Key # For encryption
+# (RSA: Short messages can be encrypted with Private and decrypted with Public)
+# 
+# 1. B sends A Key2
+# 2. A encrypts the message with B's Key2(Public) and sends it to back to B
+# 3. B decrypts it with Key1(Private)
 
 # If you are using A Mac:
 # In a terminal: brew install openssl
@@ -49,12 +60,13 @@ if (Sys.info()["sysname"] == 'Darwin'){
   print('Setting up OSX environment')
 }
 
-install.packages('PKI')
+install.packages("PKI")
 library(PKI)
 
 # Create a keypair and save them in PEM format to variables
 keypairprovider <- PKI.genRSAkey(bits = 2048L)
 
+#convert to character base
 prv.pem <- PKI.save.key(keypairprovider, private=TRUE)
 print(prv.pem)
 
@@ -96,6 +108,7 @@ print(decrypted.again)
 
 # 1. Pair up.
 # 2. Generate and exchange public keys
+
 # 3. Send one encrypted question to your peer (both of you).
 # 4. Send an encrypted answer back to your peer and decrypt the answer. 
 # 4. SAVE IT TO DISK 
